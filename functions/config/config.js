fc3a1de4bc7ec5f43f0797656ce7d101a3bdaba6
devansh dixit <devanshdixit01@gmail.com>
2020-10-03 18:55:21 +0530

    Update config.js

--- functions/config/config.js
+++ functions/config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    apiKey: "AIzaSyCxbggU-xaV7ok-LsgH-ZH8gdgzwnDP9jc",
-  authDomain: "database-664f5.firebaseapp.com",
-  databaseURL: "https://database-664f5.firebaseio.com",
-  projectId: "database-664f5",
-  storageBucket: "database-664f5.appspot.com",
-  messagingSenderId: "847821315251",
-  appId: "1:847821315251:web:24042a73254ccf9292a4c6",
-  measurementId: "G-J1RGTLRZV6"
+    apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
   };
